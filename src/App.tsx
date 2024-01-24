7d057db7065ea3c32dbd520a86812e970c0ade8a
im222b <rudgnsdleh@naver.com>
2024-01-24 15:35:21 +0900

    day08

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, DropResult, Droppable } from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "./atom";
 
 const Wrapper =styled.div `
   display: flex;
@@ -31,20 +33,33 @@ const Card = styled.div`
   border-radius: 5px;
 `;
 
-const toDos = ["a","b","c","d","e","f"];
 
 function App() {
-  const onDragEnd = () => {};
+  const [ toDos, setToDos] = useRecoilState(toDoState)
+  const onDragEnd = ({draggableId, destination, source}:DropResult) => {
+    //변경하지 않으면 원래대로
+    if (!destination) return;
+
+    setToDos(oldToDos => {
+      const ToDosCopy = [...oldToDos];
+      //기존것을 삭제   클릭한 지점 , 하나를 지운다
+      ToDosCopy.splice(source.index, 1);
+      //변경된것을 추가    변경할 위치, 지우지 않음 , 저장할 위치
+      ToDosCopy.splice(destination?.index, 0, draggableId);
+      return ToDosCopy;
+    })
+  };
+
   return (
-    
-  <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext onDragEnd={onDragEnd}>
     <Wrapper>
       <Boards>
       <Droppable droppableId="one">
         {(magic) => 
         <Board ref={magic.innerRef} {...magic.droppableProps}>
           {toDos.map((toDo,index) => (
-          <Draggable draggableId={toDo} index={index}>
+            //draggableId와 key의 값은 같아야 한다
+          <Draggable key={toDo} draggableId={toDo} index={index}>
             {(magic) => (
             <Card 
             ref={magic.innerRef} 
